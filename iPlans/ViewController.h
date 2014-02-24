16b6ddaf5a1019cd146cab20a78237a1babf9dc3
Jymn_Chen <guesn10000@126.com>
2014-02-24 23:27:34 +0800

    Initial Commit

---
+++ iPlans/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iPlans
+//
+//  Created by Jymn_Chen on 14-2-24.
+//  Copyright (c) 2014年 Jymn_Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iPlansTests/iPlansTests.m
@@ -0,0 +1,34 @@
+//
+//  iPlansTests.m
+//  iPlansTests
+//
+//  Created by Jymn_Chen on 14-2-24.
+//  Copyright (c) 2014年 Jymn_Chen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iPlansTests : XCTestCase
+
+@end
+
+@implementation iPlansTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
